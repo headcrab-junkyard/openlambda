c93252572936a8e279885755d43423b233d0ac62
BlackPhrase <BlackPhrase@users.noreply.gitlab.com>
2019-12-18 01:51:51 +0000

    Prepare the IClientGame interface implementation

---
+++ src/cl_dll/ClientGame.cpp
@@ -0,0 +1,40 @@
+/*
+ * This file is part of OpenLambda Project
+ *
+ * Copyright (C) 2019 BlackPhrase
+ *
+ * OpenLambda Project is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OpenLambda Project is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenLambda Project. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/// @file
+
+#include "ClientGame.hpp"
+
+EXPOSE_SINGLE_INTERFACE(CClientGame, IClientGame, MGT_CLIENTGAME_INTERFACE_VERSION)
+
+bool CClientGame::Init(CreateInterfaceFn afnEngineFactory)
+{
+	// TODO
+	return true;
+};
+
+void CClientGame::Shutdown()
+{
+	// TODO
+};
+
+void CClientGame::Update()
+{
+	// TODO
+};
---
+++ src/cl_dll/ClientGame.hpp
@@ -0,0 +1,33 @@
+/*
+ * This file is part of OpenLambda Project
+ *
+ * Copyright (C) 2018-2019 BlackPhrase
+ *
+ * OpenLambda Project is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OpenLambda Project is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenLambda Project. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/// @file
+
+#pragma once
+
+#include "game/client/IClientGame.hpp"
+
+class CClientGame /*final*/ : public IClientGame
+{
+public:
+	bool Init(CreateInterfaceFn afnEngineFactory /*, cldll_func_t *pcl_funcs*/) override;
+	void Shutdown() override;
+	
+	void Update() override;
+};
