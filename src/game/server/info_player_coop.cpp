9de38b3889db3a291f4604d7f444f081b0b4f089
BlackPhrase <BlackPhrase@users.noreply.gitlab.com>
2021-08-18 13:29:47 +0000

    [Game/Server] 'info_player_coop' & 'info_player_deathmatch' entities

---
+++ src/game/server/info_player_coop.cpp
@@ -0,0 +1,28 @@
+/*
+ * This file is part of OpenLambda Project
+ *
+ * Copyright (C) 1996-1997 Id Software, Inc.
+ * Copyright (C) 2021 BlackPhrase
+ *
+ * OpenLambda Project is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OpenLambda Project is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenLambda Project. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/// @file
+
+#include "PointEntity.hpp"
+
+/*QUAKED info_player_coop (1 0 1) (-16 -16 -24) (16 16 24)
+potential spawning position for coop games
+*/
+LINK_ENTITY_TO_CLASS(info_player_coop, CPointEntity);
