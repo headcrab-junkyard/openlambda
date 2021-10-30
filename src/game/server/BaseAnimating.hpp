f7b8a225a9d4b9e67d858606560837a0ce25ceec
BlackPhrase <BlackPhrase@users.noreply.gitlab.com>
2021-10-30 14:19:10 +0000

    Initial base animating entity

---
+++ src/game/server/BaseAnimating.hpp
@@ -0,0 +1,38 @@
+/*
+ * This file is part of OpenLambda Project
+ *
+ * Copyright (C) 2021 BlackPhrase
+ *
+ * OpenLambda Project is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OpenLambda Project is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenLambda Project. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/// @file
+
+#pragma once
+
+#include "BaseDelay.hpp"
+
+class CBaseAnimating : public CBaseDelay
+{
+public:
+public: // TODO: private:
+	float mfFrameRate{0.0f};
+	
+	float mfGroundSpeed{0.0f};
+	
+	float mfLastEventCheck{0.0f};
+	
+	bool mbSequenceFinished{false};
+	bool mbSequenceLoops{false};
+};
