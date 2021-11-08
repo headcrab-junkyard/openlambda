c54f43de8cc63cc4e803a0c6230f5cbbdda00f08
BlackPhrase <BlackPhrase@users.noreply.gitlab.com>
2021-11-08 13:11:07 +0000

    [Game/Server] 'info_null' entity code

---
+++ src/game/server/InfoNull.cpp
@@ -0,0 +1,35 @@
+/*
+ * This file is part of OpenLambda Project
+ *
+ * Copyright (C) 1996-1997 Id Software, Inc.
+ * Copyright (C) 2021 BlackPhrase
+ *
+ * OpenLambda Project is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OpenLambda Project is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenLambda Project. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/// @file
+
+#include "BaseEntity.hpp"
+#include "Util.hpp"
+
+/*QUAKED info_null (0 0.5 0) (-4 -4 -4) (4 4 4)
+Used as a positional target for spotlights, etc.
+*/
+class CInfoNull : public CBaseEntity
+{
+public:
+	bool PreSpawn() const override {return false;}
+};
+
+LINK_ENTITY_TO_CLASS(info_null, CInfoNull);
