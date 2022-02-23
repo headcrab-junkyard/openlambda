5b57786f16116347a8564d167fa89649c8145d72
BlackPhrase <BlackPhrase@users.noreply.gitlab.com>
2022-02-23 22:34:31 +0000

    [Dedicated] Engine execution mode interface with auto/manual impls

---
+++ src/dedicated/IEngineExecMode.hpp
@@ -0,0 +1,98 @@
+/*
+ * This file is part of OpenLambda Project
+ *
+ * Copyright (C) 2022 BlackPhrase
+ *
+ * OpenLambda Project is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OpenLambda Project is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenLambda Project. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/// @file
+/// @brief engine execution mode interface
+
+#pragma once
+
+#include "CommonTypes.hpp"
+
+#include "next/engine/IEngine.hpp"
+
+#include "engine_launcher_api.h"
+
+enum class CEngine::Result;
+
+struct IEngine::InitParams;
+
+interface IEngine;
+interface IEngineAPI;
+
+interface IEngineExecMode
+{
+    ///
+    virtual CEngine::Result Run(const IEngine::InitParams &aInitParams) = 0;
+};
+
+class CEngineExecMode_Manual final : public IEngineExecMode
+{
+public:
+    CEngineExecMode_Manual(IEngine *apEngine) : mpEngine(apEngine){}
+
+    CEngine::Result Run(const IEngine::InitParams &aInitParams) override
+    {
+        if(!Init(aInitParams))
+            return CEngine::Result::None;
+        
+        bool bRunning{true};
+        while(bRunning)
+        {
+            PreFrame();
+
+            bRunning = Frame();
+
+            PostFrame();
+        };
+
+        Shutdown();
+    };
+protected:
+    virtual void PreFrame(){}
+    virtual void PostFrame(){}
+private:
+    bool Init(const IEngine::InitParams &aInitParams)
+    {
+        return mpEngine->Init(aInitParams);
+    };
+
+    void Shutdown()
+    {
+        mpEngine->Shutdown();
+    };
+
+    bool Frame()
+    {
+        return mpEngine->Frame();
+    };
+};
+
+class CEngineExecMode_Auto final : public IEngineExecMode
+{
+public:
+    CEngineExecMode_Auto(IEngineAPI *apEngine) : mpEngine(apEngine){}
+
+    CEngine::Result Run(const IEngine::InitParams &aInitParams) override
+    {
+        // TODO
+        return mpEngine->Run();
+    };
+private:
+    IEngineAPI *mpEngine{nullptr};
+};
