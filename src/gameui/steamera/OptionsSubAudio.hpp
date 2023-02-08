743532779b63bb61b06885e23280449da346d4c4
BlackPhrase <BlackPhrase@users.noreply.gitlab.com>
2023-02-08 14:12:03 +0000

    [GameUI/SteamEra] Update the license headers

--- src/gameui/steamera/OptionsSubAudio.hpp
+++ src/gameui/steamera/OptionsSubAudio.hpp
@@ -1,20 +1,21 @@
 /*
- * This file is part of OGS Engine
+ * This file is part of OpenLambda Project
+ *
  * Copyright (C) 2018, 2020-2021, 2023 BlackPhrase
  *
- * OGS Engine is free software: you can redistribute it and/or modify
+ * OpenLambda Project is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * OGS Engine is distributed in the hope that it will be useful,
+ * OpenLambda Project is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with OGS Engine. If not, see <http://www.gnu.org/licenses/>.
- */
+ * along with OpenLambda Project. If not, see <http://www.gnu.org/licenses/>.
+*/
 
 /// @file
 /// @brief Audio details, part of options dialog
