e974dcc541858205139fe9d5cf152aaa963bf48a
BlackPhrase <BlackPhrase@users.noreply.gitlab.com>
2024-05-28 15:54:11 +0000

    [src/Next/Launcher] Load the engine modules/subsystems here

--- src/next/launcher/Application.cpp
+++ src/next/launcher/Application.cpp
@@ -1,7 +1,7 @@
 /*
  * This file is part of OGSNext Engine
  *
- * Copyright (C) 2018-2020 BlackPhrase
+ * Copyright (C) 2018-2020, 2024 BlackPhrase
  *
  * OGSNext Engine is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -22,14 +22,52 @@
 #include <stdexcept>
 
 #include "Application.hpp"
-#include "filesystem/IFileSystem.hpp"
+
+#include <next/filesystem/IFileSystem.hpp>
+#include <next/networksystem/INetworkSystem.hpp>
+#include <next/physics/IPhysics.hpp>
+#include <next/scriptsystem/IScriptSystem.hpp>
+#include <next/engineclient/IEngineClient.hpp>
+#include <next/inputsystem/IInputSystem.hpp>
+//#include <next/soundsystem/ISoundSystem.hpp>
+
+//#include <vgui3/IVGUI3.hpp>
 
 CreateInterfaceFn gfnFSFactory{nullptr};
+CreateInterfaceFn gfnNetworkSystemFactory{nullptr};
+CreateInterfaceFn gfnPhysicsFactory{nullptr};
+CreateInterfaceFn gfnScriptSystemFactory{nullptr};
+CreateInterfaceFn gfnEngineClientFactory{nullptr};
+CreateInterfaceFn gfnInputSystemFactory{nullptr};
+CreateInterfaceFn gfnSoundSystemFactory{nullptr};
+
+CreateInterfaceFn gfnVGUI3Factory{nullptr};
 
 IBaseInterface *LauncherFactory(const char *name, int *retval)
 {
 	if(!strcmp(name, OGS_FILESYSTEM_INTERFACE_VERSION))
 		return gfnFSFactory(name, retval);
+	
+	if(!strcmp(name, OGS_NETWORKSYSTEM_INTERFACE_VERSION))
+		return gfnNetworkSystemFactory(name, retval);
+	
+	if(!strcmp(name, OGS_PHYSICS_INTERFACE_VERSION))
+		return gfnPhysicsFactory(name, retval);
+	
+	if(!strcmp(name, OGS_SCRIPTSYSTEM_INTERFACE_VERSION))
+		return gfnScriptSystemFactory(name, retval);
+	
+	if(!strcmp(name, OGS_ENGINECLIENT_INTERFACE_VERSION))
+		return gfnEngineClientFactory(name, retval);
+	
+	if(!strcmp(name, OGS_INPUTSYSTEM_INTERFACE_VERSION))
+		return gfnInputSystemFactory(name, retval);
+	
+	//if(!strcmp(name, OGS_SOUNDSYSTEM_INTERFACE_VERSION))
+		//return gfnSoundSystemFactory(name, retval);
+	
+	//if(!strcmp(name, OGS_VGUI3_INTERFACE_VERSION))
+		//return gfnVGUI3Factory(name, retval);
 	
 	auto fnThisFactory{Sys_GetFactoryThis()};
 	return fnThisFactory(name, retval);
@@ -63,20 +101,43 @@ int CApplication::Run()
 
 bool CApplication::Init()
 {
+	// Try to load the file system module
 	if(!LoadFileSystemModule("filesystem_stdio"))
 		throw std::runtime_error(std::string("Failed to load the filesystem module!"));
 	
-	mpEngineLib = Sys_LoadModule("engine");
+	// Try to load the network system module
+	if(!LoadNetworkSystemModule("networksystem"))
+		throw std::runtime_error(std::string("Failed to load the network system module!"));
 	
-	if(!mpEngineLib)
-		throw std::runtime_error(std::string("Failed to load the engine module!"));
+	// Try to load the physics module
+	if(!LoadPhysicsModule("physics"))
+		throw std::runtime_error(std::string("Failed to load the physics module!"));
 	
-	auto pEngineFactory{Sys_GetFactory(mpEngineLib)};
+	// Try to load the script system module
+	if(!LoadScriptSystemModule("scriptsystem"))
+		throw std::runtime_error(std::string("Failed to load the script system module!"));
 	
-	if(!pEngineFactory)
-		return false;
+	// Try to load the engine client module
+	if(!LoadEngineClientModule("engineclient"))
+		throw std::runtime_error(std::string("Failed to load the engine client module!"));
 	
-	mpEngine = (IEngine*)pEngineFactory(OGS_ENGINE_INTERFACE_VERSION, nullptr);
+	// Try to load the input system module
+	if(!LoadInputSystemModule("inputsystem"))
+		throw std::runtime_error(std::string("Failed to load the input system module!"));
+	
+	// Try to load the sound system module
+	// TODO: -nosound?
+	//if(!LoadInputSystemModule("soundsystem"))
+		//throw std::runtime_error(std::string("Failed to load the soundsystem system module!"));
+	
+	// Try to load the VGUI3 module
+	//if(!LoadVGUI3Module("vgui3"))
+		//throw std::runtime_error(std::string("Failed to load the vgui3 module!"));
+	
+	// Try to load the engine module
+	LoadEngineModule("engine");
+	
+	mpEngine = reinterpret_cast<IEngine*>(pEngineFactory(OGS_ENGINE_INTERFACE_VERSION, nullptr));
 	
 	if(!mpEngine)
 		return false;
@@ -100,15 +161,15 @@ void CApplication::Shutdown()
 	mpEngine->Shutdown();
 };
 
-bool CApplication::LoadFileSystemModule(const char *name)
+bool CApplication::LoadFileSystemModule(const char *asName)
 {
-	if(!name || !*name)
+	if(!asName || !*asName)
 	{
-		//throw std::invalid_argument(std::string("Argument passed to CApplication::LoadFileSystemModule is invalid! (").append(name).append(")"));
+		//throw std::invalid_argument(std::string("Argument passed to CApplication::LoadFileSystemModule is invalid! (").append(asName).append(")"));
 		return false;
 	};
 	
-	mpFSLib = Sys_LoadModule(name);
+	mpFSLib = Sys_LoadModule(asName);
 	
 	if(!mpFSLib)
 		return false;
@@ -119,4 +180,166 @@ bool CApplication::LoadFileSystemModule(const char *name)
 		return false;
 	
 	return true;
+};
+
+bool CApplication::LoadNetworkSystemModule(const char *asName)
+{
+	if(!asName || !*asName)
+	{
+		//throw std::invalid_argument(std::string("Argument passed to CApplication::LoadNetworkSystemModule is invalid! (").append(asName).append(")"));
+		return false;
+	};
+	
+	mpNetworkSystemLib = Sys_LoadModule(asName);
+	
+	if(!mpNetworkSystemLib)
+		return false;
+	
+	gfnNetworkSystemFactory = Sys_GetFactory(mpNetworkSystemLib);
+	
+	if(!gfnNetworkSystemFactory)
+		return false;
+	
+	return true;
+};
+
+bool CApplication::LoadPhysicsModule(const char *asName)
+{
+	if(!asName || !*asName)
+	{
+		//throw std::invalid_argument(std::string("Argument passed to CApplication::LoadPhysicsModule is invalid! (").append(asName).append(")"));
+		return false;
+	};
+	
+	mpPhysicsLib = Sys_LoadModule(asName);
+	
+	if(!mpPhysicsLib)
+		return false;
+	
+	gfnPhysicsFactory = Sys_GetFactory(mpPhysicsLib);
+	
+	if(!gfnPhysicsFactory)
+		return false;
+	
+	return true;
+};
+
+bool CApplication::LoadScriptSystemModule(const char *asName)
+{
+	if(!asName || !*asName)
+	{
+		//throw std::invalid_argument(std::string("Argument passed to CApplication::LoadScriptSystemModule is invalid! (").append(asName).append(")"));
+		return false;
+	};
+	
+	mpScriptSystemLib = Sys_LoadModule(asName);
+	
+	if(!mpScriptSystemLib)
+		return false;
+	
+	gfnScriptSystemFactory = Sys_GetFactory(mpScriptSystemLib);
+	
+	if(!gfnScriptSystemFactory)
+		return false;
+	
+	return true;
+};
+
+bool CApplication::LoadEngineClientModule(const char *asName)
+{
+	if(!asName || !*asName)
+	{
+		//throw std::invalid_argument(std::string("Argument passed to CApplication::LoadEngineClientModule is invalid! (").append(asName).append(")"));
+		return false;
+	};
+	
+	mpEngineClientLib = Sys_LoadModule(asName);
+	
+	if(!mpEngineClientLib)
+		return false;
+	
+	gfnEngineClientFactory = Sys_GetFactory(mpEngineClientLib);
+	
+	if(!gfnEngineClientFactory)
+		return false;
+	
+	return true;
+};
+
+bool CApplication::LoadInputSystemModule(const char *asName)
+{
+	if(!asName || !*asName)
+	{
+		//throw std::invalid_argument(std::string("Argument passed to CApplication::LoadInputSystemModule is invalid! (").append(asName).append(")"));
+		return false;
+	};
+	
+	mpInputSystemLib = Sys_LoadModule(asName);
+	
+	if(!mpInputSystemLib)
+		return false;
+	
+	gfnInputSystemFactory = Sys_GetFactory(mpInputSystemLib);
+	
+	if(!gfnInputSystemFactory)
+		return false;
+	
+	return true;
+};
+
+bool CApplication::LoadSoundSystemModule(const char *asName)
+{
+	if(!asName || !*asName)
+	{
+		//throw std::invalid_argument(std::string("Argument passed to CApplication::LoadSoundSystemModule is invalid! (").append(asName).append(")"));
+		return false;
+	};
+	
+	mpSoundSystemLib = Sys_LoadModule(asName);
+	
+	if(!mpSoundSystemLib)
+		return false;
+	
+	gfnSoundSystemFactory = Sys_GetFactory(mpSoundSystemLib);
+	
+	if(!gfnSoundSystemFactory)
+		return false;
+	
+	return true;
+};
+
+bool CApplication::LoadVGUI3Module(const char *asName)
+{
+	if(!asName || !*asName)
+	{
+		//throw std::invalid_argument(std::string("Argument passed to CApplication::LoadVGUI3Module is invalid! (").append(asName).append(")"));
+		return false;
+	};
+	
+	mpVGUI3Lib = Sys_LoadModule(asName);
+	
+	if(!mpVGUI3Lib)
+		return false;
+	
+	gfnVGUI3Factory = Sys_GetFactory(mpVGUI3Lib);
+	
+	if(!gfnVGUI3Factory)
+		return false;
+	
+	return true;
+};
+
+void CApplication::LoadEngineModule(const char *asName)
+{
+	mpEngineLib = Sys_LoadModule(asName);
+	
+	if(!mpEngineLib)
+		throw std::runtime_error(std::string("Failed to load the engine module!"));
+	
+	auto pEngineFactory{Sys_GetFactory(mpEngineLib)};
+	
+	if(!pEngineFactory)
+		throw std::runtime_error(std::string("Failed to get the factory from the engine module!"));
+	
+	mpEngineFactory = pEngineFactory;
 };
--- src/next/launcher/Application.hpp
+++ src/next/launcher/Application.hpp
@@ -1,7 +1,7 @@
 /*
  * This file is part of OGSNext Engine
  *
- * Copyright (C) 2018-2020 BlackPhrase
+ * Copyright (C) 2018-2020, 2024 BlackPhrase
  *
  * OGSNext Engine is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -24,7 +24,7 @@
 #include <cstdlib>
 #include <cstring>
 
-#include "engine/IEngine.hpp"
+#include <engine/IEngine.hpp>
 
 class CApplication
 {
@@ -33,18 +33,35 @@ public:
 	~CApplication();
 	
 	int Run();
+protected:
+	virtual bool PostInit(){return true;}
 private:
 	bool Init();
 	void Shutdown();
 	
-	bool LoadFileSystemModule(const char *name);
-	
+	bool LoadFileSystemModule(const char *asName);
+	void LoadNetworkSystemModule(const char *asName);
+	void LoadPhysicsModule(const char *asName);
+	void LoadScriptSystemModule(const char *asName);
+	void LoadEngineClientModule(const char *asName);
+	void LoadInputSystemModule(const char *asName);
+	void LoadSoundSystemModule(const char *asName);
+	void LoadVGUI3Module(const char *asName);
+	void LoadEngineModule(const char *asName);
+private:
 	void *mpFSLib{nullptr};
+	void *mpNetworkSystemLib{nullptr};
+	void *mpPhysicsLib{nullptr};
+	void *mpScriptSystemLib{nullptr};
+	void *mpEngineClientLib{nullptr};
+	void *mpInputSystemLib{nullptr};
+	void *mpSoundSystemLib{nullptr};
+	void *mpVGUI3Lib{nullptr};
 	void *mpEngineLib{nullptr};
+	
+	CreateInterfaceFn mpEngineFactory{nullptr};
 	
 	IEngine *mpEngine{nullptr};
 	
 	bool mbRestart{false};
-protected:
-	virtual bool PostInit(){return true;}
 };
